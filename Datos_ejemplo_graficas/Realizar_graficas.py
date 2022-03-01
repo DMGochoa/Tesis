b5f7174f648ed35007423d77b78878778a63e24c
diego <diegomore1997@gmail.com>
2022-03-01 15:29:47 -0500

    Se agregan cambios para la realización de gráficas

--- Datos_ejemplo_graficas/Realizar_graficas.py
+++ Datos_ejemplo_graficas/Realizar_graficas.py
@@ -9,12 +9,38 @@ Ingeniería Eléctrica
 01/03/2022
 """
 
+from turtle import position
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-#def 
+sns.set()
+
+def cajas(datos=list(), xticks=list(), title='', label=tuple()):
+    """
+        Función para hacer la gráfica de cajas de cada porcentaje de entrenamiento.
+    """
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_axes([0.1, 0.1, 0.85, 0.85])
+
+    y = list()
+    x = list()
+    for i, j in zip(datos, xticks):#range(datos.__len__())
+        y.append(np.mean(i))
+        x.append(float(j))
+    ax.boxplot(datos, positions=x)
+    ax.set_xticks(x)
+    ax.set_xticklabels(x)
+    ax.plot(x, y)
+    plt.title(title, fontsize=18, fontweight="bold")
+    try:
+        plt.xlabel(label[0])
+        plt.ylabel(label[1])
+    except:
+        pass
+    plt.show()
+
 
 if __name__ == '__main__':
     # Se agregan los caminos en donde se guardan los porcentajes que se utilizaron.
@@ -23,8 +49,18 @@ if __name__ == '__main__':
 
     # Se cargan los porcentajes y luego se hace una lista con estos.
     Porcen = (open(path_core1, 'r').read() + open(path_cores, 'r').read()).replace('\n', ' ').split(' ')
-    print(Porcen)
-    Entre_model = dict()
+    Entre_model = list()
+    time_model = list()
+    #print(Porcen)
 
     for i in Porcen:
-        df = pd.read_csv('scores_para_' + i + '_porciento.csv', index_col=0)
+        df = pd.read_csv('Modelos/Modelo1/scores_para_' + i + '_porciento.csv', index_col=0)
+        df0 = pd.read_csv('Modelos/Modelo1/tiempos_para_' + i + '_porciento.csv', index_col=0)
+        Entre_model.append((df['Validaciones']*100).to_list())
+        time_model.append((df0['Tiempos']/60).to_list())
+    
+    #print(Entre_model)
+    #print(time_model)
+
+    cajas(Entre_model, xticks=Porcen, title="Score de entrenamiento", label=('Porcentaje Base de Datos Entrenamiento',
+     'Score'))
